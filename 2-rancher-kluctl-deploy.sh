b042b79befda2039093700a2ceb3bd42f18e650f
Jonathan Frisch <jfrisch46@gmail.com>
2024-06-14 13:27:53 -0400

    corrected quick start

--- 2-rancher-kluctl-deploy.sh
+++ 2-rancher-kluctl-deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-./2-kluctl-deploy.sh
+kluctl deploy -t local -y --prune
+
+kubectl wait --for=jsonpath='{.status.loadBalancer.ingress}' service/traefik -n traefik
+
+traefik_external_ip=$(kubectl get svc/traefik -n traefik -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+traefik_cluster_ip=$(kubectl get svc/traefik -n traefik -o jsonpath='{.spec.clusterIP}')
+
+export TRAEFIK_EXTERNAL_IP=${traefik_external_ip}
+export TRAEFIK_CLUSTER_IP=${traefik_cluster_ip}
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -19,3 +27,8 @@ data:
         }
     }
 EOF
+
+echo 
+echo TRAEFIK_EXTERNAL_IP: ${TRAEFIK_EXTERNAL_IP}
+echo TRAEFIK_CLUSTER_IP: ${TRAEFIK_CLUSTER_IP}
+echo 
